23f9192e7a83a93df5a1dfc01d5bfef9db066deb
MaritdeKoning <121195901+MaritdeKoning@users.noreply.github.com>
2022-12-21 21:11:23 +0100

    Add files via upload

--- Welkom.py
+++ Welkom.py
@@ -12,6 +12,6 @@ st.header('Welkom')
 
 st.text('Op deze website kunt U busplanningen checken hier krijgt U er een overzichtelijk \nrooster bij. Hieronder in de afbeelding kunt U zien welke route de bussen door \nEindhoven rijden.')
 
-image1 = Image.open(r'C:/Users/Petra/OneDrive - Office 365 Fontys/Toegepaste wiskunde/Jaar 2/Project/Project 5/programmeren/Busritten kaart.png')
+image1 = Image.open(r'Busritten kaart.png')
 st.image(image1, caption='Busrit 400 en busrit 401', width = 600, output_format= 'PNG')
 
